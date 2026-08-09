e0761b1a94cf22b6b10a75ae82a4a3c6006d6dd6
Zhihao LIU <7567311+liuh886@users.noreply.github.com>
2026-08-09 16:17:05 +0800

    Remove obsolete invite redemption compatibility migration

--- supabase/migrations/20260809161500_membership_invite_redemption_pair.sql
+++
@@ -1,8 +0,0 @@
-alter table public.membership_invites
-  drop constraint if exists membership_invites_redemption_pair;
-
-alter table public.membership_invites
-  add constraint membership_invites_redemption_pair check (
-    (redeemed_by is null and redeemed_at is null)
-    or (redeemed_by is not null and redeemed_at is not null)
-  );
